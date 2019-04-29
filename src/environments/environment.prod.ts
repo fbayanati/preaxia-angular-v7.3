dcf6163a6e1fffcdc51cbba1e1999a66a4c4d2d7
unknown <fbayanati@yahoo.com>
2019-04-28 22:43:05 -0600

    add firebase web configuration settings in environment classes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyDNwvZP_Oh-HvFy0u9gD51Fm_wOtyxzCGE",
+    authDomain: "preaxia-dev.firebaseapp.com",
+    databaseURL: "https://preaxia-dev.firebaseio.com",
+    projectId: "preaxia-dev",
+    storageBucket: "preaxia-dev.appspot.com",
+    messagingSenderId: "392581029896"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDNwvZP_Oh-HvFy0u9gD51Fm_wOtyxzCGE",
+    authDomain: "preaxia-dev.firebaseapp.com",
+    databaseURL: "https://preaxia-dev.firebaseio.com",
+    projectId: "preaxia-dev",
+    storageBucket: "preaxia-dev.appspot.com",
+    messagingSenderId: "392581029896"
+  }
 };
 
 /*
