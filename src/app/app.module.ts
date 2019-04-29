bcf2ff2939f77ed8c2dc78d4e9fb622afb53fed6
unknown <fbayanati@yahoo.com>
2019-04-28 22:40:27 -0600

    add @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { PreaxiaFooterComponent } from './ui/preaxia-footer/preaxia-footer.component';
 import { PreaxiaNavComponent } from './ui/preaxia-nav/preaxia-nav.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +22,11 @@ import { PreaxiaNavComponent } from './ui/preaxia-nav/preaxia-nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule 
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
