e552201242510cf2c1999f52bb58d5c41ee37a82
unknown <fbayanati@yahoo.com>
2019-04-28 22:46:36 -0600

    initialize firebase ui with AngularFireAuth to provide Email/Password and Gmail authentication

--- src/app/preaxia-login/preaxia-login/preaxia-login.component.ts
+++ src/app/preaxia-login/preaxia-login/preaxia-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-preaxia-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreaxiaLoginComponent implements OnInit {
 
-  constructor() { }
+  ui: firebaseui.auth.AuthUI;
+
+  constructor(
+    private afAuth: AngularFireAuth
+  ) {
+    // TODO :: whatever necessary!
+  }
 
   ngOnInit() {
+
+    const uiConfig = {
+      signInOptions: [
+        // The providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult:
+          this.onLoginSuccessful.bind(this)
+          // Process result. This will not trigger on merge conflicts.
+          // On success redirect to signInSuccessUrl.
+      }
+    }
+
+    this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
+    this.ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  onLoginSuccessful() {
+
   }
 
 }
