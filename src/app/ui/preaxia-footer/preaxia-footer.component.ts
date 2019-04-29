655f96266995a99f2b8f6d7fb2f89bc88b406d19
unknown <fbayanati@yahoo.com>
2019-04-28 19:17:53 -0600

    add text for preaxia footer component

--- src/app/ui/preaxia-footer/preaxia-footer.component.ts
+++ src/app/ui/preaxia-footer/preaxia-footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreaxiaFooterComponent implements OnInit {
 
+  year: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear()
   }
 
 }
