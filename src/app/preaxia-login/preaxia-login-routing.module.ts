e165b5f864380a390b167c4e22cf67aefbaf22f6
unknown <fbayanati@yahoo.com>
2019-04-28 22:43:34 -0600

    add routes for login

--- src/app/preaxia-login/preaxia-login-routing.module.ts
+++ src/app/preaxia-login/preaxia-login-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreaxiaLoginComponent } from './preaxia-login/preaxia-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: PreaxiaLoginComponent
+  },
+  {
+      path: '**',
+      redirectTo: '',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
