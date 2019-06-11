16e1a0257696d8a5885eaaee4cb2299f9d097461
unknown <fbayanati@yahoo.com>
2019-06-10 21:51:36 -0600

    update registration component

--- src/app/registration/components/registration/registration.component.ts
+++ src/app/registration/components/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AccountType } from 'src/app/model/account';
 
 @Component({
   selector: 'app-registration',
@@ -8,9 +10,22 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public acountType = AccountType;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  registerAccount(accountType: string) {
+    console.log( 'registerAccount :: ', accountType );
+    this.router.navigate(['/registration/account', { type: accountType }]);
+  }
+
+  accountType() {
+    return AccountType;
   }
 
 }
