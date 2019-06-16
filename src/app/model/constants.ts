35a3f85d09e627985799710e94bde5d60578e3ce
unknown <fbayanati@yahoo.com>
2019-06-16 15:15:51 -0600

    add constants class and static array of provinces

---
+++ src/app/model/constants.ts
@@ -0,0 +1,19 @@
+export class Constants {
+
+    static readonly provinces = [
+        'Alberta',
+        'British Columbia',
+        'Manitoba',
+        'New Brunswick',
+        'Newfoundland and Labrador',
+        'Northwest Territories',
+        'Nova Scotia',
+        'Nunavut',
+        'Ontario',
+        'Prince Edward Island',
+        'Quebec',
+        'Saskatchewan',
+        'Yukon'
+    ]
+
+};
